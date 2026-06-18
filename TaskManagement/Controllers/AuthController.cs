169e294bc7ff2125280b1ddaff9f85c69472872f
Awf Alhalaybeh <Awf Alhalaybeh@DESKTOP-OQV6N10>
2026-06-18 21:16:06 +0300

    ..

--- TaskManagement/Controllers/AuthController.cs
+++ TaskManagement/Controllers/AuthController.cs
@@ -4,7 +4,6 @@ using TaskManagement.Services;
 
 namespace TaskManagement.Controllers
 {
-    //
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
